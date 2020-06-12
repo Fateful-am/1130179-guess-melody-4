9e4f3efa749be2490a45807090945a06c2d14a4f
Fateful-am <fateful.am@gmail.com>
2020-06-12 11:34:52 +0300

    ДЕМО. Ограничит jest проекта, чтобы не было конфликтов с jest демок

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,15 +1,11 @@
-import React from "react";
-import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
-
+import React from 'react';
+import WelcomeScreen from '../welcome-screen/welcome-screen.jsx';
 
 const App = (props) => {
   // eslint-disable-next-line react/prop-types
   const {errorsCount} = props;
 
-  return (
-    <WelcomeScreen errorsCount={errorsCount} />
-  );
+  return <WelcomeScreen errorsCount={errorsCount} />;
 };
-
 
 export default App;
--- src/components/welcome-screen/welcome-screen.jsx
+++ src/components/welcome-screen/welcome-screen.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-
+import React from 'react';
 
 const WelcomeScreen = (props) => {
   // eslint-disable-next-line react/prop-types
@@ -8,9 +7,11 @@ const WelcomeScreen = (props) => {
   return (
     <section className="welcome">
       <div className="welcome__logo">
-        <img src="img/melody-logo.png" alt="Угадай мелодию" width="186" height="83"/>
+        <img src="img/melody-logo.png" alt="Угадай мелодию" width="186" height="83" />
       </div>
-      <button className="welcome__button"><span className="visually-hidden">Начать игру</span></button>
+      <button className="welcome__button">
+        <span className="visually-hidden">Начать игру</span>
+      </button>
       <h2 className="welcome__rules-title">Правила игры</h2>
       <p className="welcome__text">Правила просты:</p>
       <ul className="welcome__rules-list">
@@ -21,6 +22,5 @@ const WelcomeScreen = (props) => {
     </section>
   );
 };
-
 
 export default WelcomeScreen;
